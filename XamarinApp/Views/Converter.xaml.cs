5743875a27b3a92fb5a071b65e98086c0999f1be
kittymichael <kittymichaelpineda@gmail.com>
2023-04-11 11:51:00 -0500

    Add project files.

---
+++ XamarinApp/Views/Converter.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Converter : ContentPage
+	{
+		double cm;
+		double m;
+
+		public Converter ()
+		{
+			InitializeComponent ();//shows us the frontend part
+        }
+		private void CalculateLength()
+		{
+			cm = Convert.ToDouble(txtcm.Text);
+			m = cm / 100;
+			lbloutcome.Text = m.ToString() + " m";
+		}
+		private void Validate()
+		{
+			if(!string.IsNullOrEmpty(txtcm.Text))
+			{
+				CalculateLength();
+			}
+			else
+			{
+				DisplayAlert("Error", "Please enter a value for conversion","ok");
+			}
+		}
+        private void btnback_Clicked(object sender, EventArgs e)
+        {
+			Navigation.PopAsync();	
+        }
+
+        private void btnconvert_Clicked(object sender, EventArgs e)
+        {
+			Validate();
+
+        }
+    }
+}
---
+++ XamarinApp/Views/MainForm.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainForm : ContentPage
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnstart_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Converter());
+
+        }
+    }
+}
